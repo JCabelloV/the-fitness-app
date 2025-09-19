d18fc5ccc2c42de78e62803a8a25e061802e6d48
JuanCabelloVargas <jicabello@miuandes.cl>
2025-09-19 13:26:20 -0300

    feat(db): Create User model and table, also fixed trainer model file

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_09_18_161136) do
+ActiveRecord::Schema[8.0].define(version: 2025_09_19_161552) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -18,6 +18,15 @@ ActiveRecord::Schema[8.0].define(version: 2025_09_18_161136) do
     t.string "first_name", null: false
     t.string "last_name", null: false
     t.integer "rut", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "first_name", null: false
+    t.string "last_name", null: false
+    t.integer "rut", null: false
+    t.string "email", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
