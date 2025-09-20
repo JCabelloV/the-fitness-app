cc85b96aaf274d52c7ca44289444f3f843a0a50a
JCabelloV <cabellovargasj@gmail.com>
2025-09-19 22:08:11 -0300

    feat(db): Create bookings table

---
+++ db/migrate/20250920002634_create_bookings_table.rb
@@ -0,0 +1,15 @@
+class CreateBookingsTable < ActiveRecord::Migration[8.0]
+  def change
+    create_table :bookings do |t|
+      t.references :schedule, null: false, foreign_key: true
+      t.references :trainer, null: false, foreign_key: true
+
+      t.integer :kind, null: false, default: 0
+      t.integer :status, null: false, default: 0
+
+      t.integer :price, null: false, default: 0
+
+      t.timestamps
+    end
+  end
+end
